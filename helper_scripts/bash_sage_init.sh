1be71ed9f965bad810c9df776ac2b84893dd7994
Maarten Derickx <maarten@mderickx.nl>
2021-11-26 12:46:00 +0100

    Use sage root command

--- helper_scripts/bash_sage_init.sh
+++ helper_scripts/bash_sage_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # source this script to get an environment with a python 3
 # where sourcing importing from sage works
-SAGE_BIN_DIR=$(sage -sh -c 'echo $SAGE_LOCAL')/bin
+SAGE_BIN_DIR=$(sage -sh -c 'sage -root')/bin
 source ${SAGE_BIN_DIR}/sage-env-config
 source ${SAGE_BIN_DIR}/sage-env
