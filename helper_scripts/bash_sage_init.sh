837a6894569046d3a6ac5877460aac6a7249c39b
M. Derickx <derickxm@capella.backup.leidenuniv.nl>
2021-12-26 12:28:47 +0100

    Make isogeny primes work with sage installed from sourceWq

--- helper_scripts/bash_sage_init.sh
+++ helper_scripts/bash_sage_init.sh
@@ -3,6 +3,9 @@
 # where sourcing importing from sage works
 HELPER_DIR=$(dirname -- "${BASH_SOURCE[0]}")
 SAGE_BIN_DIR=$(sage -sh -c 'echo $SAGE_LOCAL')/bin
+if [ ! -f ${SAGE_BIN_DIR}/sage-env-config ]; then
+    SAGE_BIN_DIR=$(sage -sh -c 'echo $SAGE_VENV')/bin
+fi
 source ${SAGE_BIN_DIR}/sage-env-config
 source ${SAGE_BIN_DIR}/sage-env
-export PATH=$(dirname -- ${HELPER_DIR})/venv/bin:${PATH}
+export PATH=$(dirname -- ${HELPER_DIR})/venv/bin:$SAGE_BIN_DIR:${PATH}
