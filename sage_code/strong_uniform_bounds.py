9a17bc9342fd1047527f8eea498123d5102885de
barinderbanwait <b.s.banwait86@gmail.com>
2023-01-27 15:27:04 -0500

    wip

--- sage_code/strong_uniform_bounds.py
+++ sage_code/strong_uniform_bounds.py
@@ -31,7 +31,8 @@
 from sage.all import Partitions, divisors, matrix, GF, lcm
 from itertools import product
 
-from .common_utils import get_weil_polys
+from common_utils import get_weil_polys
+from pil_integers import collapse_tuple
 
 def is_non_increasing(t):
 
@@ -39,6 +40,12 @@ def is_non_increasing(t):
         if t[j] < t[j+1]:
             return False
     return True
+
+
+def get_beta_mats_with_pow(F, pow=1):
+
+    frob_polys = get_weil_polys(F)
+    return [matrix.companion(f) ** pow for f in frob_polys]
 
 
 def remove_duplicates(list_of_e_tuples):
@@ -75,7 +82,7 @@ def splitting_types(d):
     return types
 
 
-def strong_mult_bound(d, eps, q):
+def bound_from_split_type(split_type, eps, q):
     """This implements Algorithm 5.1 from the paper, and returns the integers
        B_{eps,q} and B^*_{eps,q}.
 
@@ -84,16 +91,29 @@ def strong_mult_bound(d, eps, q):
         eps (tuple): isogeny signature
         q (int): auxiliary rational prime
     """
-
-    frob_polys = get_weil_polys(GF(q))
-    frob_poly_mats = [matrix.companion(a_frob_poly) for a_frob_poly in frob_polys]
-    matrix_parent = frob_poly_mats[0].parent()
+    frob_poly_mats = [get_beta_mats_with_pow(GF(q**f), pow=my_e) for my_e,f in zip(split_type['es'],split_type['fs'])]
+    beta_mat_tuples = list(product(*frob_poly_mats))
+    collapsed_beta_mats = [
+        collapse_tuple(a_beta_tuple) for a_beta_tuple in beta_mat_tuples
+    ]
     tr_eps = sum(eps)
-    alpha_to_eps_mat = matrix_parent(q**tr_eps)
-    B_eps_q = 1
-    for a_beta_mat in frob_poly_mats:
+    q_to_tr_eps = q**tr_eps
+    running_lcm = 1
+    for a_beta_mat in collapsed_beta_mats:
+        matrix_parent = frob_poly_mats[0].parent()
+        alpha_to_eps_mat = matrix_parent(q_to_tr_eps)
         pil_mat = alpha_to_eps_mat.tensor_product(a_beta_mat.parent()(1)) - (
                   alpha_to_eps_mat.parent()(1)).tensor_product(a_beta_mat)
         pil_int = pil_mat.det()
-        B_eps_q = lcm(pil_int, B_eps_q)
-    return B_eps_q
+        running_lcm = lcm(pil_int, running_lcm)
+    return running_lcm
+
+
+def B_eps_q(d,eps,q):
+
+    split_types = splitting_types(d)
+    B = 1
+    for split_type in split_types:
+        pil_int = bound_from_split_type(split_type, eps, q)
+        B = lcm(B,pil_int)
+    return B
